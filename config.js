adbf265ba2c02b04b08ba105a31501f782777fcc
Gaurav Pandey <gauravpandey495@gmail.com>
2021-10-02 11:00:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 const orgname = "CodeWithGauri";
 const message ="Let's Crack it!"
-const logo_url = 'Webdev_Hacktober/Projects/188863115_1465622990446090_853368353958307680_n.jpg';  //your logo url  -> img/filename.extension
+const logo_url = './img/logo.png';  //your logo url  -> img/filename.extension
 const github_username ="codewithgauri"   //your username
 const github_reponame = "Webdev_Hacktober"; //your reponame
 const linkedin = "gaurav-pandey-a5b884131/"    // your linkedin Username
